4c6ee84ae5a843ca190a74ed4746ae7bbfc50eaa
Pedro Moitinho <pedromoitinhoprado@gmail.com>
2024-08-05 17:52:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ for (var i=0; i<drumbuttons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", botaoclique);
     function botaoclique(){
         var buttonInnerHTML = this.innerHTML;
-        
         makeSound(buttonInnerHTML);
-        buttonAnimation(buttonInnerHTML);
-
     }
 }
 document.addEventListener("keypress", function(event) {
     makeSound(event.key);
-    buttonAnimation(buttonInnerHTML);
 
 })
 function makeSound(key){
@@ -46,15 +42,4 @@ function makeSound(key){
             audio.play();
         break;
     }
-    function buttonAnimation(currentKey) {
-
-        var activeButton = document.querySelector("." + currentKey);
-      
-        activeButton.classList.add("pressed");
-      
-        setTimeout(function() {
-          activeButton.classList.remove("pressed");
-        }, 100);
-      
-      }
 }
