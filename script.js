2abf8f8f7c18f5f98a9cd79b7c51624c2c5b2ab5
Pedro Moitinho <pedromoitinhoprado@gmail.com>
2024-08-07 15:21:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,63 @@
-@import url('https://fonts.googleapis.com/css2?family=Arvo:ital,wght@0,400;0,700;1,400;1,700&family=Bona+Nova+SC:ital,wght@0,400;0,700;1,400&display=swap');
-*{
-    margin:0;
-    box-sizing:border-box;
-    font-family: "Arvo", cursive;
-    
-}
-html{
-    background-color:rgb(39,42,67);
-}
-#title{
-    width:100vw;
-    display:flex;
-    justify-content:center;
-    margin-top:2%;
-    font-size:400%;
-    text-shadow: 3px 1px #DA0463;
-    color: #DBEDF3;
-    text-align:center;
-}
-.set{
-    width:100%;
-    display:flex;
-    justify-content:center;
-    gap:2%;
-    overflow:hidden;
-    margin:10% auto;
-    flex-wrap:wrap;
-    row-gap:2vh;
-    
-    
-    
-}
-.drum{
-    display:flex;
-    width:150px;
-    height:150px;
-    border:10px solid #404B69;
-    border-radius:15px;
-    background-color:rgb(255,253,255);
-    text-align:center;
-    color:rgb(215,0,91);
-    font-size:600%;
-    align-items:end;
-    justify-content:center;
-    
-    
-    
-}
-@media(max-width:600px){
-    .drum{
-        width:100px;
-        height:100px;
-        font-size:400%;
+
+var drumbuttons = document.querySelectorAll(".drum").length;
+for (var i=0; i<drumbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", botaoclique);
+    function botaoclique(){
+        var buttonInnerHTML = this.innerHTML;
+        
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
     }
-    .set{
-        row-gap:2vh;
+}
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(buttonInnerHTML);
+
+})
+function makeSound(key){
+    switch(key){
+        case "w":
+            var crash =new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+        case "a":
+            var kick =new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+        break;
+        case "s":
+            var snare =new Audio("./sounds/snare.mp3");
+            snare.play();
+        break;
+        case "d":
+            var tom1 =new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+        case "j":
+            var tom2 =new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        case "k":
+            var tom3 =new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+        case "l":
+            var tom4 =new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+        default: console.log(key);
     }
+    function buttonAnimation(currentKey) {
+
+        var activeButton = document.querySelector("." + currentKey);
+      
+        activeButton.classList.add("pressed");
+      
+        setTimeout(function() {
+          activeButton.classList.remove("pressed");
+        }, 100);
+      
+      }
+      
 
 }
-footer{
-    display:flex;
-    justify-content:center;
-    margin-top:20vh;
-    color: #DBEDF3;
-    font-size:85%;
-    text-decoration:underline rgba(219,234,242,0.4);
-}
-.drum:hover{
-    background-color:rgb(190,190,190);
-}
-.pressed {
-    box-shadow: 0 3px 4px 0 #DBEDF3;
-    opacity: 0.5;
-  }
